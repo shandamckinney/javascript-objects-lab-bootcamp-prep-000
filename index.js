d34975a1d03988ff0be5a392f87a7cf1b217769e
shandamckinney <mckinney.shanda@gmail.com>
2018-05-09 18:23:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
+
   return Object.assign({}, object, { [key]: value })
 
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  delete object([key] : value)
 }
