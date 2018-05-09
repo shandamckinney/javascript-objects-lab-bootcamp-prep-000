50b20316a2a71d07f5dc6793797f7c8acd7c485a
shandamckinney <mckinney.shanda@gmail.com>
2018-05-09 18:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+  delete object.key;
+  return obj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
